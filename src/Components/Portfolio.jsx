75851d093272fd64726093445a949aa44258ec02
nikola4work <nikola4work@outlook.com>
2020-04-26 20:55:04 -0400

    new cards for portfolio display

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Styles/Portfolio.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Container, Row, Col} from "react-bootstrap";
 import burger from "../Components/Images/burger.gif";
 import codeQuiz from "../Components/Images/codeQuiz.gif";
 import goat from "../Components/Images/goat.gif";
@@ -11,148 +12,164 @@ import weather from "../Components/Images/weather.gif";
 function Portfolio() {
   return (
     <div className="container-fluid portfolio">
-      <div className="container project">
-        <div className="row">
-          <div className="col-md-3 project-work">
-            {" "}
-            <img src={goat} alt="Avatar" className="image"></img>
-            <div className="middle">
-              <div className="text">
-                <h3 className="work-title">G.O.A.T Kicks</h3>
-                <p className="work-text">
+      <Container className="team">
+        <Row className="style">
+          <Col lg={4}>
+            <div className="our-team-main">
+              <div className="team-front">
+                <img src={goat} alt="" className="img-fluid" />
+              </div>
+
+              <div className="team-back">
+                <span>
+                  <h2 className="work-title">G.O.A.T Kicks</h2>
+                  <br />
                   Goat is a website where users can browse and filter trough
                   latest sneakers.
-                </p>
-                <form>
-                  <button
-                    className="button"
-                    formaction="https://ashkiani.github.io/GOAT/home.html"
-                    formTarget="_blank"
-                  >
-                    Live Preview
-                  </button>
-                </form>
+                  <form>
+                    <button
+                      className="button"
+                      formaction="https://ashkiani.github.io/GOAT/home.html"
+                      formTarget="_blank"
+                    >
+                      Live Preview
+                    </button>
+                  </form>
+                </span>
               </div>
             </div>
-          </div>
-          <div className="col-md-3 project-work">
-            {" "}
-            <img src={virtualfair} alt="Avatar" className="image"></img>
-            <div className="middle">
-              <div className="text">
-                <h3 className="work-title">Virtual Fair</h3>
-                <p className="work-text">
+          </Col>
+
+          <Col lg={4}>
+            <div className="our-team-main">
+              <div className="team-front">
+                <img src={virtualfair} alt="" className="img-fluid" />
+              </div>
+
+              <div className="team-back">
+                <span>
+                  <h2 className="work-title">Virtual Fair</h2>
+                  <br />
                   This initial version of the project will allow you browse a
                   list of jobs and apply for those jobs.
-                </p>
-                <form>
-                  <button
-                    className="button"
-                    formaction="https://aqueous-cliffs-33447.herokuapp.com/"
-                    formTarget="_blank"
-                  >
-                    Live Preview
-                  </button>
-                </form>
+                  <form>
+                    <button
+                      className="button"
+                      formaction="https://aqueous-cliffs-33447.herokuapp.com/"
+                      formTarget="_blank"
+                    >
+                      Live Preview
+                    </button>
+                  </form>
+                </span>
               </div>
             </div>
-          </div>
-          <div className="col-md-3 project-work">
-            {" "}
-            <img src={codeQuiz} alt="Avatar" className="image"></img>
-            <div className="middle">
-              <div className="text">
-                {" "}
-                <h3 className="work-title">Code Quiz</h3>
-                <p className="work-text">
+          </Col>
+
+          <Col lg={4}>
+            <div className="our-team-main">
+              <div className="team-front">
+                <img src={codeQuiz} alt="" className="img-fluid" />
+              </div>
+
+              <div className="team-back">
+                <span>
+                  <h2 className="work-title">Code Quiz</h2>
+                  <br />
                   Simple quiz game runs on JavaScript where users can test their
-                  coding Knowledge
-                </p>
-                <form>
-                  <button
-                    className="button"
-                    formaction="https://nikola4work.github.io/CodeQuiz-JS/"
-                    formTarget="_blank"
-                  >
-                    Live Preview
-                  </button>
-                </form>
+                  coding Knowledge.
+                  <form>
+                    <button
+                      className="button"
+                      formaction="https://nikola4work.github.io/CodeQuiz-JS/"
+                      formTarget="_blank"
+                    >
+                      Live Preview
+                    </button>
+                  </form>
+                </span>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="row" id="media">
-          <div className="col-md-3 project-work">
-            {" "}
-            <img src={notes} alt="Avatar" className="image"></img>
-            <div className="middle">
-              <div className="text">
-                {" "}
-                <h3 className="work-title">Notes App</h3>
-                <p className="work-text">
+          </Col>
+
+          <Col lg={4} className="margin">
+            <div className="our-team-main">
+              <div className="team-front">
+                <img src={notes} alt="" className="img-fluid" />
+              </div>
+
+              <div className="team-back">
+                <span>
+                  <h2 className="work-title">Notes App</h2>
+                  <br />
                   An application that can be used to write, save, and delete
                   notes. Used Heroku to deploy the app.
-                </p>
-                <form>
-                  <button
-                    className="button"
-                    formaction="http://niko-notes.herokuapp.com/"
-                    formTarget="_blank"
-                  >
-                    Live Preview
-                  </button>
-                </form>
+                  <form>
+                    <button
+                      className="button"
+                      formaction="http://niko-notes.herokuapp.com/"
+                      formTarget="_blank"
+                    >
+                      Live Preview
+                    </button>
+                  </form>
+                </span>
               </div>
             </div>
-          </div>
-          <div className="col-md-3 project-work">
-            {" "}
-            <img src={burger} alt="Avatar" className="image"></img>
-            <div className="middle">
-              <div className="text">
-                {" "}
-                <h3 className="work-title">Eat-Da-Burger</h3>
-                <p className="work-text">
+          </Col>
+
+          <Col lg={4} className="margin">
+            <div className="our-team-main">
+              <div className="team-front">
+                <img src={weather} alt="" className="img-fluid" />
+              </div>
+
+              <div className="team-back">
+                <span>
+                  <h2 className="work-title">Weather Dashboard</h2>
+                  <br />
+                  Weather Dashboard build on 3rd party API - where users can
+                  check current & 5 days weather forecast.
+                  <form>
+                    <button
+                      className="button"
+                      formaction="https://nikola4work.github.io/WeatherForecast/"
+                      formTarget="_blank"
+                    >
+                      Live Preview
+                    </button>
+                  </form>
+                </span>
+              </div>
+            </div>
+          </Col>
+          <Col lg={4} className="margin">
+            <div className="our-team-main">
+              <div className="team-front">
+                <img src={burger} alt="" className="img-fluid" />
+              </div>
+
+              <div className="team-back">
+                <span>
+                  <h2 className="work-title">Eat-Da-Burger</h2>
+                  <br />
                   Eat-Da-Burger is a restaurant app that lets users input the
                   names of burgers they'd like to eat!
-                </p>
-                <form>
-                  <button
-                    className="button"
-                    formaction="https://burger--app.herokuapp.com/"
-                    formTarget="_blank"
-                  >
-                    Live Preview
-                  </button>
-                </form>
+                  <form>
+                    <button
+                      className="button"
+                      formaction="https://burger--app.herokuapp.com/"
+                      formTarget="_blank"
+                    >
+                      Live Preview
+                    </button>
+                  </form>
+                </span>
               </div>
             </div>
-          </div>
-          <div className="col-md-3 project-work">
-            {" "}
-            <img src={weather} alt="Avatar" className="image"></img>
-            <div className="middle">
-              <div className="text">
-                {" "}
-                <h3 className="work-title">Weather Dashboard</h3>
-                <p className="work-text">
-                  Weather Dashboard build on 3rd party API - where users can
-                  check current & 5 days weather forecast
-                </p>
-                <form>
-                  <button
-                    className="button"
-                    formaction="https://nikola4work.github.io/WeatherForecast/"
-                    formTarget="_blank"
-                  >
-                    Live Preview
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
